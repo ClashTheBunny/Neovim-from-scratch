721bae5a6c98e0f07ab009e749bbb6bee94c9edd
Randall Mason <randall@mason.ch>
2022-06-07 21:55:25 +0000

    Fix for latest, include cider if available

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -83,6 +83,8 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-buffer" -- buffer completions
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "hrsh7th/vim-vsnip"
+  use "hrsh7th/vim-vsnip-integ"
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
 
