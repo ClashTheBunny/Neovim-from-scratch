bd93c514ffcfcd2185aab432d7c4bc4497168aa2
Randall Mason <Randall@Mason.CH>
2022-07-15 12:28:07 -0500

    Add puppet syntax highlighting

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -103,11 +103,12 @@ return packer.startup(function(use)
 	use({ "nvim-telescope/telescope.nvim", commit = "d96eaa914aab6cfc4adccb34af421bdd496468b0" })
   use "mileszs/ack.vim"
 
-	-- Treesitter
+	-- Treesitter and other syntax highlighting
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		commit = "518e27589c0463af15463c9d675c65e464efc2fe",
 	})
+  use("rodjek/vim-puppet")
 
   -- Git
 	use({ "lewis6991/gitsigns.nvim", commit = "c18e016864c92ecf9775abea1baaa161c28082c3" })
